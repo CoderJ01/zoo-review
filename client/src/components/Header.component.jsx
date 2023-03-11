b2963739a98ea39b6032cf217161ad6d9acbac85
CoderJ01 <buildingguy@sbcglobal.net>
2023-03-11 13:32:25 -0800

    rearranage order or header content

--- client/src/components/Header.component.jsx
+++ client/src/components/Header.component.jsx
@@ -5,18 +5,12 @@ const Header = () => {
     return (
         <div>
             <nav>
-                <NavLink to='/'><h1>Zelp</h1></NavLink>
-                <input></input>
-                <ul>
-                    <li>
-                      <NavLink to='/post-blog'>Post a Blog</NavLink>
-                    </li>
-                    <li>
-                      <NavLink to='/write-review'>Write a Review</NavLink>
-                    </li>
-                </ul>
+                <NavLink to='/'>Zelp</NavLink>
+                <NavLink to='/post-blog'>Post a Blog</NavLink>
+                <NavLink to='/write-review'>Write a Review</NavLink>
                 <button>Log In</button>
                 <button>Sign Up</button>
+                <input></input>
             </nav>
             <Outlet/>
         </div>
