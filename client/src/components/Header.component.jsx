6c3bfd79b24d8749bb0c1d23bad2a03023ec33dc
CoderJ01 <buildingguy@sbcglobal.net>
2023-03-10 21:27:51 -0800

    create header contents

--- client/src/components/Header.component.jsx
+++ client/src/components/Header.component.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import { Outlet, NavLink } from 'react-router-dom'; 
 
 const Header = () => {
     return (
-        <div></div>
+        <div>
+            <nav>
+                <NavLink to='/'><h1>Zelp</h1></NavLink>
+                <input></input>
+                <ul>
+                    <li>
+                      <NavLink to='/post-blog'>Post a Blog</NavLink>
+                    </li>
+                    <li>
+                      <NavLink to='/write-review'>Write a Review</NavLink>
+                    </li>
+                </ul>
+                <button>Log In</button>
+                <button></button>
+            </nav>
+            <Outlet/>
+        </div>
     );
 }
 
