53e55655b32f7177299a75a7175eea5bba08393b
CoderJ01 <buildingguy@sbcglobal.net>
2023-03-11 19:20:52 -0800

    set up division structure of review

--- client/src/components/Review/Review.component.jsx
+++ client/src/components/Review/Review.component.jsx
@@ -4,7 +4,18 @@ import './Review.style.css'
 const Review = () => {
     return (
         <div className='review'>
+            <div className='review-heading'>
+                <div className='review-avatar'>
 
+                </div>
+                <div>
+                    <div className='review-username'></div>
+                    <text>Wrote a review</text>
+                </div>
+            </div>
+            <div className='review-picture'></div>
+            <div className='review-info'></div>
+            <div className='review-icons'></div>
         </div>
     );
 }
