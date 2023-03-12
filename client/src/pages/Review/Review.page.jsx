4d9cacbe0a8dfad4cd3ed5da34b37a162826531d
CoderJ01 <buildingguy@sbcglobal.net>
2023-03-12 15:16:04 -0700

    add useState functionality to form

--- client/src/pages/Review/Review.page.jsx
+++ client/src/pages/Review/Review.page.jsx
@@ -1,21 +1,32 @@
 import React from 'react';
+import { useState } from 'react';
 
 const Review = () => {
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(title)
+        console.log(content)
+    }
+
     return (
         <div className='review'>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <h1>Write Your Review</h1>
                 <div>
                     <label htmlFor='title'>Title:</label><br/>
-                    <input type="text" name="title" />
+                    <input type="text" name="title" onChange={(e) => setTitle(e.target.value)}/>
                 </div>
                 <div>
                     <label htmlFor='content'>Content:</label><br/>
-                    <input type="text" name="content" />
+                    <input type="text" name="content" onChange={(e) => setContent(e.target.value)}/>
                 </div>
                 <div>
                     <text>Upload image here</text>
                 </div>
+                <input type="submit" />
             </form>
         </div>
     );
