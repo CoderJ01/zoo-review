0cfe70c4d54eda1e507d86cc7185db203dd5faea
CoderJ01 <buildingguy@sbcglobal.net>
2023-03-12 14:42:50 -0700

    create review form

--- client/src/pages/Review/Review.page.jsx
+++ client/src/pages/Review/Review.page.jsx
@@ -2,8 +2,18 @@ import React from 'react';
 
 const Review = () => {
     return (
-        <div>
-
+        <div className='review'>
+            <h1>Write Your Review</h1>
+            <form>
+                <div>
+                    <label htmlFor='title'>Title:</label><br/>
+                    <input type="text" name="title" />
+                </div>
+                <div>
+                    <label htmlFor='content'>Content:</label><br/>
+                    <input type="text" name="content" />
+                </div>
+            </form>
         </div>
     );
 }
