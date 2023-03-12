310c1629d5b0ec9e49c12cef576be7755d864f19
CoderJ01 <buildingguy@sbcglobal.net>
2023-03-12 14:22:33 -0700

    import Review to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Home from './pages/Home/Home.page';
 import Header from './components/Header/Header.component';
+import Review from './pages/Review/Review.page';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Header/>}>
             <Route index element={<Home/>}/>
+            <Route path='/write-review' element={<Review/>}/>
           </Route>
         </Routes>
       </BrowserRouter>
