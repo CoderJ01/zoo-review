439135ec34316c5db2a02174e56843b6d50fe6f3
CoderJ01 <buildingguy@sbcglobal.net>
2023-03-12 19:39:43 -0700

    import

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Home from './pages/Home/Home.page';
 import Header from './components/Header/Header.component';
 import Review from './pages/Review/Review.page';
+import Blog from './pages/Blog/Blog.page';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
           <Route path='/' element={<Header/>}>
             <Route index element={<Home/>}/>
             <Route path='/write-review' element={<Review/>}/>
+            <Route path='/post-blog' element={<Blog/>}/>
           </Route>
         </Routes>
       </BrowserRouter>
