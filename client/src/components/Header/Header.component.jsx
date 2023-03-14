e0bba2f95aa9d99538ae6aca5e0848211fa6bfe8
CoderJ01 <buildingguy@sbcglobal.net>
2023-03-13 17:17:28 -0700

    style demo modal from React Bootstrap

--- client/src/components/Header/Header.component.jsx
+++ client/src/components/Header/Header.component.jsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Outlet, NavLink } from 'react-router-dom'; 
 import './Header.style.css'
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 const Header = () => {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return (
         <header>
             <nav>
@@ -13,8 +20,37 @@ const Header = () => {
                     <NavLink to='/dashboard'><text>Dashboard</text></NavLink>
                 </div>
                 <div className='header-buttons'>
-                    <button>Log In</button>
-                    <button>Sign Up</button>
+                    {/* <button>Log In</button> */}
+                    {/* <button>Sign Up</button> */}
+                    <div className='header-vanilla-button'>
+                        <Button 
+                            style={{
+                                backgroundColor: 'white', 
+                                border: '1px solid white',
+                                borderRadius: '10px',
+                                color: 'rgb(29, 146, 34)',
+                                height: '100%', 
+                                width: '100%' 
+                            }} 
+                            onClick={handleShow}
+                        >
+                            <div className='header-vanilla-button-text'>Launch demo modal</div>
+                        </Button>
+                    </div>
+                    <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Modal heading</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={handleClose}>
+                        Close
+                        </Button>
+                        <Button variant="primary" onClick={handleClose}>
+                        Save Changes
+                        </Button>
+                    </Modal.Footer>
+                    </Modal>
                 </div>
                 <input></input>
             </nav>
