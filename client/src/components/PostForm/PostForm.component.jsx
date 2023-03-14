c08bb3ffdb87d032252e327285f36fbcd7736396
CoderJ01 <buildingguy@sbcglobal.net>
2023-03-13 18:47:16 -0700

    clean

--- client/src/components/PostForm/PostForm.component.jsx
+++ client/src/components/PostForm/PostForm.component.jsx
@@ -36,11 +36,11 @@ const PostForm = ({ formHeading, buttonText, blog = false}) => {
             <h1>{formHeading}</h1>
             <div>
                 <label htmlFor='title'>Title:</label><br/>
-                <input type="text" name="title" value={title} onChange={(e) => setTitle(e.target.value)}/>
+                <input type='text' name='title' value={title} onChange={(e) => setTitle(e.target.value)}/>
             </div>
             <div>
                 <label htmlFor='content'>Content:</label><br/>
-                <input type="text" name="content" value={content} onChange={(e) => setContent(e.target.value)}/>
+                <input type='text' name='content' value={content} onChange={(e) => setContent(e.target.value)}/>
             </div>
             {
                 !blog ? 
@@ -65,9 +65,9 @@ const PostForm = ({ formHeading, buttonText, blog = false}) => {
             }
             <div>
                 <label htmlFor='file'></label><br/>
-                <input type="file" name="file" onChange={(e) => setFile(e.target.files[0])}/>
+                <input type='file' name='file' onChange={(e) => setFile(e.target.files[0])}/>
             </div>
-            <button type="submit">+ {buttonText}</button>
+            <button type='submit'>+ {buttonText}</button>
         </form>
     );
 }
