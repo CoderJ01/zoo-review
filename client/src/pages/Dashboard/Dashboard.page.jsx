378480ac821b3592800e38ee6c3b2bbbab32a093
CoderJ01 <buildingguy@sbcglobal.net>
2023-03-13 20:04:36 -0700

    create classNames in Dashboard JSX

--- client/src/pages/Dashboard/Dashboard.page.jsx
+++ client/src/pages/Dashboard/Dashboard.page.jsx
@@ -2,7 +2,24 @@ import React from 'react';
 
 const Dashboard = () => {
     return (
-        <div></div>
+        <div className='dashboard'>
+            <div className='dashboard-top-bar'>
+                <h1>Your Dashboard</h1>
+                <div className='dashboard-top-bar-buttons'>
+                    <button>See your reviews</button>
+                    <button>See your blog posts</button>
+                </div>
+            </div>
+            <div className='dashboard-bottom'>
+                <div className='dashboard-bottom-user-info'>
+                    <div className='dashboard-bottom-user-info-avatar'></div>
+                    <div className='dashboard-bottom-user-info-bio'></div>
+                </div>
+                <div classname='dasboard-bottom-posts-display'>
+                    <div className='dasboard-bottom-posts-display-single-post'></div>
+                </div>
+            </div>
+        </div>
     );
 }
 
