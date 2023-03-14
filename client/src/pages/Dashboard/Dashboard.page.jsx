4cf48cc91e9603f7820dea01f3f71dd529201cdd
CoderJ01 <buildingguy@sbcglobal.net>
2023-03-13 20:06:53 -0700

    shorten classnames

--- client/src/pages/Dashboard/Dashboard.page.jsx
+++ client/src/pages/Dashboard/Dashboard.page.jsx
@@ -5,18 +5,18 @@ const Dashboard = () => {
         <div className='dashboard'>
             <div className='dashboard-top-bar'>
                 <h1>Your Dashboard</h1>
-                <div className='dashboard-top-bar-buttons'>
+                <div className='dashboard-t-b-buttons'>
                     <button>See your reviews</button>
                     <button>See your blog posts</button>
                 </div>
             </div>
             <div className='dashboard-bottom'>
-                <div className='dashboard-bottom-user-info'>
-                    <div className='dashboard-bottom-user-info-avatar'></div>
-                    <div className='dashboard-bottom-user-info-bio'></div>
+                <div className='dashboard-b-user-info'>
+                    <div className='dashboard-b-u-i-avatar'></div>
+                    <div className='dashboard-b-u-i-bio'></div>
                 </div>
-                <div classname='dasboard-bottom-posts-display'>
-                    <div className='dasboard-bottom-posts-display-single-post'></div>
+                <div classname='dasboard-b-posts-display'>
+                    <div className='dasboard-b-p-d-single-post'></div>
                 </div>
             </div>
         </div>
