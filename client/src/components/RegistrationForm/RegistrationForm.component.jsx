8688ded5e23a8480c8edc58b1fd38dde3f998f28
CoderJ01 <buildingguy@sbcglobal.net>
2023-03-13 17:40:23 -0700

    style modal button

--- client/src/components/RegistrationForm/RegistrationForm.component.jsx
+++ client/src/components/RegistrationForm/RegistrationForm.component.jsx
@@ -2,19 +2,22 @@ import React from 'react';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 
+const buttonStyle = {
+    backgroundColor: 'rgb(34, 191, 41)', 
+    border: '2px solid rgb(34, 191, 41)', 
+    color: 'white'
+}
+
 const RegistrationForm = ({ showSignup, handleCloseSignup }) => {
     return (
         <Modal show={showSignup} onHide={handleCloseSignup}>
             <Modal.Header closeButton>
-                <Modal.Title>Modal heading</Modal.Title>
+                <Modal.Title>Register Here!</Modal.Title>
             </Modal.Header>
             <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
             <Modal.Footer>
-                <Button variant="secondary" onClick={handleCloseSignup}>
-                Close
-                </Button>
-                <Button variant="primary" onClick={handleCloseSignup}>
-                Save Changes
+                <Button style={buttonStyle} onClick={handleCloseSignup}>
+                    Sign up
                 </Button>
             </Modal.Footer>
         </Modal>
