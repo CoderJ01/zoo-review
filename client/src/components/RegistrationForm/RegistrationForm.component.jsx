d8e39b4cedd5ede60c1217c4df79b44d447a90b4
CoderJ01 <buildingguy@sbcglobal.net>
2023-03-13 17:28:51 -0700

    place modal in RegistrationForm component

--- client/src/components/RegistrationForm/RegistrationForm.component.jsx
+++ client/src/components/RegistrationForm/RegistrationForm.component.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
 
-const RegistrationForm = () => {
+const RegistrationForm = ({ showSignup, handleCloseSignup }) => {
     return (
-        <div>Test modal</div>
+        <Modal show={showSignup} onHide={handleCloseSignup}>
+            <Modal.Header closeButton>
+                <Modal.Title>Modal heading</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={handleCloseSignup}>
+                Close
+                </Button>
+                <Button variant="primary" onClick={handleCloseSignup}>
+                Save Changes
+                </Button>
+            </Modal.Footer>
+        </Modal>
     );
 }
 
