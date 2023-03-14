20687735260ef9dc96571d7840bfdcf92b5214b8
CoderJ01 <buildingguy@sbcglobal.net>
2023-03-13 19:32:26 -0700

    create login form

--- client/src/components/LoginForm/LoginForm.jsx
+++ client/src/components/LoginForm/LoginForm.jsx
@@ -1,8 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import '../RegistrationForm/RegistrationForm.style.css';
 
-const LoginForm = () => {
+const modalHeaderStyle = {
+    display: 'flex', 
+    justifyContent: 'center'
+}
+
+const buttonStyle = {
+    backgroundColor: 'rgb(34, 191, 41)', 
+    border: '2px solid rgb(34, 191, 41)', 
+    color: 'white',
+    marginLeft: '80%',
+    marginTop: '3vh'
+}
+
+
+const LoginForm = ({ showLogin, handleCloseLogin }) => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault(); 
+    }
+
     return (
-        <div></div>
+        <Modal show={showLogin} onHide={handleCloseLogin}>
+            <Modal.Header style={modalHeaderStyle}>
+                <Modal.Title>Login Here!</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <form className='registration-form' onSubmit={handleSubmit}>
+                    <div>
+                        <label htmlFor='username'>Username:</label><br/>
+                        <input type='text' name='username' value={username} onChange={(e) => setUsername(e.target.value)}/>
+                    </div>
+                    <div>
+                        <label htmlFor='passowrd'>Password:</label><br/>
+                        <input type='text' name='password' value={password} onChange={(e) => setPassword(e.target.value)}/>
+                    </div>
+                    <Button type='submit' style={buttonStyle} onClick={handleCloseLogin}>
+                        Login in
+                    </Button>
+                </form>
+            </Modal.Body>
+        </Modal>
     );
 }
 
