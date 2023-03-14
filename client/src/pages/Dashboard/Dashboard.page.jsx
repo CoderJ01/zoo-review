f1fc407d6c4f86e37ff354e92fbf976cef6a8c36
CoderJ01 <buildingguy@sbcglobal.net>
2023-03-14 12:59:36 -0700

    move division

--- client/src/pages/Dashboard/Dashboard.page.jsx
+++ client/src/pages/Dashboard/Dashboard.page.jsx
@@ -6,15 +6,15 @@ const Dashboard = () => {
         <div className='dashboard'>
             <div className='dashboard-top-bar'>
                 <h1>Your Dashboard</h1>
-                <div className='dashboard-t-b-buttons'>
-                    <button>See your reviews</button>
-                    <button>See your blog posts</button>
-                </div>
             </div>
             <div className='dashboard-bottom'>
                 <div className='dashboard-b-user-info'>
                     <div className='dashboard-b-u-i-avatar'></div>
                     <div className='dashboard-b-u-i-bio'></div>
+                </div>
+                <div className='dashboard-b-buttons'>
+                    <button>See your reviews</button>
+                    <button>See your blog posts</button>
                 </div>
                 <div classname='dasboard-b-posts-display'>
                     <div className='dasboard-b-p-d-single-post'></div>
