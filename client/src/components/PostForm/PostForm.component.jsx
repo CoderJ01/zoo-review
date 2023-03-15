cb9fa796d293de0a76652afc76349e479d372fc5
CoderJ01 <buildingguy@sbcglobal.net>
2023-03-14 17:13:50 -0700

    clean

--- client/src/components/PostForm/PostForm.component.jsx
+++ client/src/components/PostForm/PostForm.component.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 import { ratings } from './PostForm.utils';
 import './PostForm.style.css'
 
-const PostForm = ({ formHeading, buttonText, blog = false}) => {
+const PostForm = () => {
     const [title, setTitle] = useState('');
     const [content, setContent] = useState('');
     const [file, setFile] = useState('');
@@ -13,27 +13,26 @@ const PostForm = ({ formHeading, buttonText, blog = false}) => {
     const handleSubmit = (event) => {
         event.preventDefault();
 
-        if(!blog) {
-            const url = 'http://localhost:3000/write-review';
-            const formData = new FormData();
-    
-            formData.append('file', file);
-            formData.append('fileName', file.name);
-    
-            const config = {
-                headers: {
-                    'content-type': 'multipart/form-data',
-                },
-            };
-    
-            axios.post(url, formData, config).then((response) => {
-                console.log(response.data);
-            });
-        }
+        const url = 'http://localhost:3000/write-review';
+        const formData = new FormData();
+
+        formData.append('file', file);
+        formData.append('fileName', file.name);
+
+        const config = {
+            headers: {
+                'content-type': 'multipart/form-data',
+            },
+        };
+
+        axios.post(url, formData, config).then((response) => {
+            console.log(response.data);
+        });
     }
+
     return (
         <form className='post-form' onSubmit={handleSubmit}>
-            <h1>{formHeading}</h1>
+            <h1>Write a Review</h1>
             <div>
                 <label htmlFor='title'>Title:</label><br/>
                 <input type='text' name='title' value={title} onChange={(e) => setTitle(e.target.value)}/>
@@ -42,32 +41,24 @@ const PostForm = ({ formHeading, buttonText, blog = false}) => {
                 <label htmlFor='content'>Content:</label><br/>
                 <textarea type='text' name='content' value={content} onChange={(e) => setContent(e.target.value)}/>
             </div>
-            {
-                !blog ? 
-                ( 
-                    <div>
-                        <label htmlFor='rating'>Rating:</label><br/>
-                        <select 
-                            value={selected} 
-                            onChange={e => setSelected(e.target.value)}
-                        >
-                            {ratings.map((value) => (
-                            <option value={value} key={value}>
-                                {value}
-                            </option>
-                            ))}
-                        </select>
-                    </div>
-                ) : 
-                (
-                    ''
-                )
-            }
+                <div>
+                    <label htmlFor='rating'>Rating:</label><br/>
+                    <select 
+                        value={selected} 
+                        onChange={e => setSelected(e.target.value)}
+                    >
+                        {ratings.map((value) => (
+                        <option value={value} key={value}>
+                            {value}
+                        </option>
+                        ))}
+                    </select>
+                </div>
             <div>
                 <label htmlFor='file'></label><br/>
                 <input type='file' name='file' onChange={(e) => setFile(e.target.files[0])}/>
             </div>
-            <button type='submit'>+ {buttonText}</button>
+            <button type='submit'>+ New Review</button>
         </form>
     );
 }
