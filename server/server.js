3ef7fcf5ecb31a758f57c66485937e593176a749
CoderJ01 <buildingguy@sbcglobal.net>
2023-03-14 18:44:21 -0700

    connect

--- server/server.js
+++ server/server.js
@@ -2,11 +2,14 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 require('colors');
+const connectDB = require('./config/connection.js');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(cors());
+
+connectDB();
 
 app.listen(PORT, console.log(`Listing on port: ${PORT}...`))
