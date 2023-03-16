16873cc1366e3862bce991e43710c6d13634426b
CoderJ01 <buildingguy@sbcglobal.net>
2023-03-15 19:51:51 -0700

    import route to express server

--- server/server.js
+++ server/server.js
@@ -2,14 +2,17 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 require('colors');
-const connectDB = require('./config/connection.js');
+const userRoutes = require('../server/routes/user');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const connectDB = require('./config/connection.js');
 
 app.use(express.json());
 app.use(cors());
 
 connectDB();
+
+app.use(userRoutes)
 
 app.listen(PORT, console.log(`Listing on port: ${PORT}...`))
