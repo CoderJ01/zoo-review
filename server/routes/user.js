30a97abc9cafee9f8df3b8c654b9837bcc7b4508
CoderJ01 <buildingguy@sbcglobal.net>
2023-03-15 19:51:02 -0700

    POST route for user

---
+++ server/routes/user.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+
+router.post('/user', (req, res) => {
+    const newUser =  new User({
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+        bio: req.body.bio,
+        avatar: req.body.avatar
+    });
+    newUser.save();
+    res.send(newUser);
+});
+
+module.exports = router;
