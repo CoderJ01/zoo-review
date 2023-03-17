2b672fb98e3eb36c0c0794ba3b7f09d809c43358
CoderJ01 <buildingguy@sbcglobal.net>
2023-03-17 15:55:55 -0700

    create route for blogs

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./user');
 const reviewRoutes = require('./review');
+const blogRoutes = require('./blog');
 
 router.use('/users', userRoutes);
 router.use('/reviews', reviewRoutes);
+router.use('/blogs', blogRoutes);
 
 module.exports = router;
