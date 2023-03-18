995c7d2bd292aa7c5ccd9bb250172a7e1cafa752
CoderJ01 <buildingguy@sbcglobal.net>
2023-03-17 20:42:22 -0700

    push review to zoo object

--- server/routes/review.js
+++ server/routes/review.js
@@ -3,18 +3,25 @@ const mongoose = require('mongoose');
 const ReviewSchema = require('../models/Review');
 const Review = mongoose.model('Review', ReviewSchema);
 const User = require('../models/User');
+const Zoo = require('../models/Zoo');
 
-router.post('/:userId', async (req, res) => {
+router.post('/:userId/:zooId', async (req, res) => {
     const review = new Review({
         title: req.body.title,
         content: req.body.content,
         rating: req.body.rating,
         image: req.body.image,
-        user: req.params.userId
+        user: req.params.userId,
+        zoo: req.params.zooId
     });
     review.save();
 
     await User.findOneAndUpdate({ _id: req.params.userId },
+        { $push: { 'reviews': review } },
+        { new: true }
+    );
+
+    await Zoo.findOneAndUpdate({ _id: req.params.zooId },
         { $push: { 'reviews': review } },
         { new: true }
     );
