b00cbdb4d5c2bcb4f1f43a92e700522504314aff
CoderJ01 <buildingguy@sbcglobal.net>
2023-03-17 19:31:49 -0700

    PATCH route for zoos

--- server/routes/zoo.js
+++ server/routes/zoo.js
@@ -18,6 +18,27 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
     const zoo = await Zoo.findById({ _id: req.params.id });
     res.send(zoo);
+});
+
+router.patch('/:id', async (req, res) => {
+    try {
+        const zoo = await Zoo.findOne({ _id: req.params.id });
+
+        if(req.body.name) {
+            zoo.name = req.body.name;
+        }
+
+        if(req.body.image) {
+            zoo.image = req.body.image;
+        }
+
+        zoo.save();
+        res.send(zoo);
+    }
+    catch {
+        res.status(404);
+        res.send({ error: 'Post does not exists!'});
+    }
 })
 
 module.exports = router;
