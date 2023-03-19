480155639027fae42a7287f60ce0e28d4bef26c1
CoderJ01 <buildingguy@sbcglobal.net>
2023-03-19 13:11:19 -0700

    set up connect-mongodb-session store

--- server/server.js
+++ server/server.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 const cors = require('cors');
 require('dotenv').config();
 require('colors');
 const routes = require('../server/routes/index');
+
+const MAX_AGE = 1000 * 60 * 60 * 3;
+
+const mongoDBstore = new MongoDBStore({
+    uri: process.env.MONGO_URI,
+    collection: 'mySession'
+});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
