b8d11dc66713a525eee42c7e6550159b65e6ab11
CoderJ01 <buildingguy@sbcglobal.net>
2023-03-19 13:42:38 -0700

    clean

--- server/routes/user.js
+++ server/routes/user.js
@@ -20,27 +20,6 @@ router.post('/register', async (req, res) => {
     res.send(newUser);
 });
 
-router.post('/login', async (req, res) => {
-    try {
-        const user = await User.findOne({ username: req.body.username });
-        if(!user) {
-            res.status(400).json('User does not exist!');
-        }
-
-        const validate = await bcrypt.compare(req.body.password, user.password);
-        if(!validate) {
-            res.status(400).json('Wrong password!');
-        }
-
-        const { password, ...other } = user._doc;
-        res.status(200).json(other);
-        res.send(other);
-    }
-    catch(error) {
-        res.status(500).json(error)
-    }
-});
-
 router.get('/', async (req, res) => {
     const users = await User.find();
     res.send(users);
