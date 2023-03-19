4ac3082a659406beeffc288dd3ef6f8d80cfdaa5
CoderJ01 <buildingguy@sbcglobal.net>
2023-03-19 13:30:47 -0700

    configure session

--- server/server.js
+++ server/server.js
@@ -4,23 +4,48 @@ const MongoDBStore = require('connect-mongodb-session')(session);
 const cors = require('cors');
 require('dotenv').config();
 require('colors');
-const routes = require('../server/routes/index');
-
-const mongoDBstore = new MongoDBStore({
-    uri: process.env.MONGO_URI,
-    collection: 'mySession'
-});
+const routes = require('./routes');
+const loginRouter = require('./routes/login');
 
 const app = express();
 const MAX_AGE = 1000 * 60 * 60 * 3;
 const PORT = process.env.PORT || 3001;
 const connectDB = require('./config/connection.js');
 
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200 // for legacy browser
+}
+
+mongoose.Promise = global.Promise;
+connectDB();
+
+const mongoDBstore = new MongoDBStore({
+    uri: process.env.MONGO_URI,
+    collection: 'mySession'
+});
+
+app.use(
+    session({
+        secret: process.env.SECRET,
+        name: 'session-id',
+        store: mongoDBstore,
+        cookie: {
+            maxAge: MAX_AGE,
+            sameSite: false,
+            secure: false
+        },
+        resave: true,
+        saveUninitialized: false
+    })
+);
+
 app.use(express.json());
 app.use(cors());
 
-connectDB();
-
+app.use('/users', loginRouter);
 app.use(routes);
 
-app.listen(PORT, console.log(`Listing on port: ${PORT}...`))
+app.listen(PORT, console.log(`Listing on port: ${PORT}...`));
+
+module.exports = app;
