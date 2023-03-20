60b737be9414bbcbac576360576b326159385a64
CoderJ01 <buildingguy@sbcglobal.net>
2023-03-19 20:57:58 -0700

    DELETE route to for logout functionality

--- server/routes/user.js
+++ server/routes/user.js
@@ -97,6 +97,22 @@ router.delete('/:id', async (req, res) => {
         res.status(404);
         res.send({ error: 'User does not exist' });
     }
-})
+});
+
+router.delete('/logout', (req, res) => {
+    if(req.session) {
+        req.session.destroy(err => {
+            if(err) {
+                res.status(400).send('Unable to log out!');
+            } 
+            else {
+                res.send('Logout successful!')
+            }
+        });
+    }
+    else {
+        res.end();
+    }
+});
 
 module.exports = router;
