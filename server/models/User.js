954989f394b470b0719d3145d1a06f8aa467dd81
CoderJ01 <buildingguy@sbcglobal.net>
2023-03-19 18:33:18 -0700

    disable username and email from not being unique

--- server/models/User.js
+++ server/models/User.js
@@ -14,11 +14,13 @@ const UserSchema = new mongoose.Schema(
         },
         username: {
             type: String,
-            required: true
+            required: true, 
+            unique: true
         }, 
         email: {
             type: String,
-            required: true
+            required: true,
+            unique: true
         },
         password: {
             type: String,
