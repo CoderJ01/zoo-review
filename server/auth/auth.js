0bf8d1aafcb8affd9d1edb570b125f7fd07d54e9
CoderJ01 <buildingguy@sbcglobal.net>
2023-03-20 15:51:31 -0700

    create function to require user authentication

---
+++ server/auth/auth.js
@@ -0,0 +1,8 @@
+export const requireAuth = (req, res, next) => {
+    const { user } = req.session;
+    if(!user) {
+        return res
+            .status(401).
+            json({ message: 'Unauthorized '});
+    }
+}
