73853f5173b6cc22229d5b9464f7e94d898567f0
CoderJ01 <buildingguy@sbcglobal.net>
2023-03-21 13:39:00 -0700

    clean

--- server/server.js
+++ server/server.js
@@ -1,45 +1,12 @@
 const express = require('express');
-// const session = require('express-session');
-const mongoose = require('mongoose');
-// const MongoDBStore = require('connect-mongodb-session')(session);
-// const cors = require('cors');
 require('dotenv').config();
 require('colors');
 const routes = require('./routes');
 
 const app = express();
-// const MAX_AGE = 1000 * 60 * 60 * 3;
 const PORT = process.env.PORT || 3001;
 const connectDB = require('./config/connection.js');
-
-// const corsOptions = {
-//     origin: 'http://localhost:3000',
-//     optionsSuccessStatus: 200 // for legacy browser
-// }
-
-mongoose.Promise = global.Promise;
 connectDB();
-
-// const mongoDBstore = new MongoDBStore({
-//     uri: process.env.MONGO_URI,
-//     collection: 'mySession'
-// });
-
-// app.use(
-//     session({
-//         secret: process.env.SECRET,
-//         name: 'session-id',
-//         store: mongoDBstore,
-//         cookie: {
-//             maxAge: MAX_AGE,
-//             sameSite: false,
-//             secure: false,
-//             httpOnly: true
-//         },
-//         resave: true,
-//         saveUninitialized: false
-//     })
-// );
 
 app.use(express.json());
 app.use(cors());
