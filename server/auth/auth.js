978c6347c80c80f778a46234d8b8554b6e86f2dc
CoderJ01 <buildingguy@sbcglobal.net>
2023-03-20 18:17:47 -0700

    change

--- server/auth/auth.js
+++ server/auth/auth.js
@@ -2,8 +2,8 @@ export const requireAuth = (req, res, next) => {
     const { user } = req.session;
     if(!user) {
         return res
-            .status(401).
-            json({ message: 'Unauthorized '});
+            .status(401)
+            .json({ message: 'Unauthorized '});
     }
     next();
 }
