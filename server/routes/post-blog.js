59fcdb3cfb77f81fc7c3d003bed3f28569aeb780
CoderJ01 <buildingguy@sbcglobal.net>
2023-03-21 13:06:06 -0700

    add route for blog-post page

---
+++ server/routes/post-blog.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const mongoose = require('mongoose');
+const BlogSchema = require('../../models/Blog');
+const Blog = mongoose.model('Blog', BlogSchema);
+const User = require('../../models/User');
+
+router.post('/post-blog/:userId', async (req, res) => {
+    const blog = await Blog.create({
+        title: req.body.title,
+        content: req.body.content,
+        image: req.body.image,
+        user: req.params.userId
+    });
+    blog.save();
+    res.send(blog);
+
+    await User.findByIdAndUpdate(
+        { _id: req.params.userId },
+        { $push: { 'blogs': blog }},
+        { new: true } 
+    );
+});
+
+module.exports = router;
