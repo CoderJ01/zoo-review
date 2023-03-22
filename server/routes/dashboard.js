7fbcbcd2e22d2285b1e6cc315f7236711f0f4440
CoderJ01 <buildingguy@sbcglobal.net>
2023-03-21 20:32:16 -0700

    clean

--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -7,31 +7,11 @@ const Review = mongoose.model('Review', ReviewSchema);
 const Blog = mongoose.model('Blog', BlogSchema);
 
 router.get('/reviews', async (req, res) => {
-    try {
-        const { user }  = req.session;
-        console.log(req.session);
-        const userReviews = await Review.find({
-            user: user._id
-        });
-        res.send(userReviews);
-    }
-    catch(err) {
-        return res.status(400).json({ error: err });
-    }
+  
 });
 
 router.get('/blogs', async (req, res) => {
-    try {
-        const { user }  = req.session;
-        console.log(req.session);
-        const userBlogs = await Blog.find({
-            user: user._id
-        });
-        res.send(userBlogs);
-    }
-    catch(err) {
-        return res.status(400).json({ error: err });
-    }
+    
 });
 
 module.exports = router;
