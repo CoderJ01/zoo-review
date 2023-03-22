d382fb83a75a15eb3981481fb7b699f60d6f72c8
CoderJ01 <buildingguy@sbcglobal.net>
2023-03-21 19:01:07 -0700

    implement express-session in main server

--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 require('dotenv').config();
 require('colors');
 const cors = require('cors');
+const session = require('express-session');
+const MongoDBstore = require('connect-mongodb-session')(session);
+const mongoose = require('mongoose');
 const routes = require('./routes');
 
 const app = express();
@@ -9,8 +12,31 @@ const PORT = process.env.PORT || 3001;
 const connectDB = require('./config/connection.js');
 connectDB();
 
+const mongoDBstore = new MongoDBstore({
+    uri: process.env.MONGO_URI,
+    collection: 'mySessions'
+});
+
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
+
+const maxAge = 1000 * 60 * 60 * 3; 
+
+app.use(
+    session({
+      name: 'express-cookie', 
+      secret: process.env.SECRET,
+      resave: true,
+      saveUninitialized: false,
+      store: mongoDBstore,
+      cookie: {
+        maxAge: maxAge,
+        sameSite: false,
+        secure: true
+      }
+    })
+  );
 
 app.use(routes);
 
