060b50d1484697bdb7580d41f2ffef3f745e6389
CoderJ01 <buildingguy@sbcglobal.net>
2023-03-22 17:51:41 -0700

    enable function to retrieve id as a parameter

--- server/MongoDB/data.js
+++ server/MongoDB/data.js
@@ -2,12 +2,12 @@ const { MongoClient } = require('mongodb');
 const uri = process.env.MONGO_URI;
 const client = new MongoClient(uri);
 
-async function retrieveSession() {
+async function retrieveSession(id) {
     try {
         await client.connect()
         const db = client.db(process.env.DB);
         const coll = db.collection('mySessions');
-        const cursor = coll.find({ 'session.user.id': ''});
+        const cursor = coll.find({ 'session.user.id': id });
         await cursor.forEach(console.log);
     }
     finally {
