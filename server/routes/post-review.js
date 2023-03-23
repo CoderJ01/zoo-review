d45811e6b3fefd1c0d39ef4ffff40628ba6890e4
CoderJ01 <buildingguy@sbcglobal.net>
2023-03-22 18:22:51 -0700

    ditto for reviews

--- server/routes/post-review.js
+++ server/routes/post-review.js
@@ -4,28 +4,40 @@ const ReviewSchema = require('../../models/Review');
 const Review = mongoose.model('Review', ReviewSchema);
 const User = require('../../models/User');
 const Zoo = require('../../models/Zoo');
+const retrieveSession = require('../MongoDB/data');
 
 router.post('/post-review/:userId/:zooId', async (req, res) => {
-    const review = new Review({
-        title: req.body.title,
-        content: req.body.content,
-        rating: req.body.rating,
-        user: req.params.userId,
-        zoo: req.params.zooId
-    });
-    review.save();
-
-    await User.findOneAndUpdate({ _id: req.params.userId },
-        { $push: { 'reviews': review } },
-        { new: true }
-    );
-
-    await Zoo.findOneAndUpdate({ _id: req.params.zooId },
-        { $push: { 'reviews': review } },
-        { new: true }
-    );
-
-    res.send(review);
+    try {
+        let user = retrieveSession(req.params.userId);
+        if(user) {
+            const review = new Review({
+                title: req.body.title,
+                content: req.body.content,
+                rating: req.body.rating,
+                user: req.params.userId,
+                zoo: req.params.zooId
+            });
+            review.save();
+        
+            await User.findOneAndUpdate({ _id: req.params.userId },
+                { $push: { 'reviews': review } },
+                { new: true }
+            );
+        
+            await Zoo.findOneAndUpdate({ _id: req.params.zooId },
+                { $push: { 'reviews': review } },
+                { new: true }
+            );
+        
+            res.send(review);
+        }
+    }
+    catch(err) {
+        res.send({
+            error: err,
+            msg: 'Invalid authentication!'
+        });
+    }
 });
 
 module.exports = router;
