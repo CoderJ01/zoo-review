3fb5aada3877e48c5df18c770e9fae32e181283c
CoderJ01 <buildingguy@sbcglobal.net>
2023-03-22 18:07:14 -0700

    enable reviews in dashboard to be seen by user only

--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -7,8 +7,21 @@ const Review = mongoose.model('Review', ReviewSchema);
 const Blog = mongoose.model('Blog', BlogSchema);
 const retrieveSession = require('../MongoDB/data');
 
-router.get('/reviews', async (req, res) => {
-    
+router.get('/reviews/:userId', async (req, res) => {
+    try {
+        let user = retrieveSession(req.params.userId);
+        console.log(user);
+        if(user) {
+            let review = await Review.find({ user: req.params.userId });
+            res.send(review);
+        }
+    }
+    catch(err) {
+        res.send({
+            error: err,
+            msg: 'Invalid authentication!'
+        });
+    }
 });
 
 router.get('/blogs', async (req, res) => {
