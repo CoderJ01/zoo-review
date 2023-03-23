5a2ffe02d0dd6a414dc0c10c934855826670fac9
CoderJ01 <buildingguy@sbcglobal.net>
2023-03-22 18:17:44 -0700

    allow only logged in users to post a blog

--- server/routes/post-blog.js
+++ server/routes/post-blog.js
@@ -3,22 +3,34 @@ const mongoose = require('mongoose');
 const BlogSchema = require('../../models/Blog');
 const Blog = mongoose.model('Blog', BlogSchema);
 const User = require('../../models/User');
+const retrieveSession = require('../MongoDB/data');
 
 router.post('/post-blog/:userId', async (req, res) => {
-    const blog = await Blog.create({
-        title: req.body.title,
-        content: req.body.content,
-        image: req.body.image,
-        user: req.params.userId
-    });
-    blog.save();
-    res.send(blog);
-
-    await User.findByIdAndUpdate(
-        { _id: req.params.userId },
-        { $push: { 'blogs': blog }},
-        { new: true } 
-    );
+    try {
+        let user = retrieveSession(req.params.userId);
+        if(user) {
+            const blog = await Blog.create({
+                title: req.body.title,
+                content: req.body.content,
+                image: req.body.image,
+                user: req.params.userId
+            });
+            blog.save();
+            res.send(blog);
+        
+            await User.findByIdAndUpdate(
+                { _id: req.params.userId },
+                { $push: { 'blogs': blog }},
+                { new: true } 
+            );
+        }
+    }
+    catch(err) {
+        res.send({
+            error: err,
+            msg: 'Invalid authentication!'
+        })
+    }
 });
 
 module.exports = router;
