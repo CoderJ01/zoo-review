0e1f8de73b0da7e31f8399825edcc108de6985de
CoderJ01 <buildingguy@sbcglobal.net>
2023-03-22 19:14:42 -0700

    fix errors

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
-const { retrieveSession } = require('../MongoDB/data');
+const { retrieveSession, deleteSession } = require('../MongoDB/data');
 
 router.post('/register', async (req, res) => {
     const salt = await bcrypt.genSalt(10);
