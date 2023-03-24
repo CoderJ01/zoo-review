0ff7fbdb9ebe1f9cfaa738f360d408f2be1dae84
CoderJ01 <buildingguy@sbcglobal.net>
2023-03-23 22:11:51 -0700

    set up axios GET in client for blogs

--- client/src/pages/Home/Home.page.jsx
+++ client/src/pages/Home/Home.page.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
 import Post from '../../components/Post/Post.component';
 import './Home.style.css'
+import axios from 'axios';
+
+const baseURL = 'http://localhost:3001';
+
+let blogs = [];
+axios.get(baseURL + '/homepage/blogs')
+.then(
+    response =>  {
+        console.log(response.data.length)
+        for(let i = 0; i < response.data.length; i++) {
+            // blogs.push(response.data[i]);
+            blogs[i] = response.data[i];
+        }
+    })
+.catch(err => console.log(err));
+
+console.log(blogs);
 
 const Home = () => {
+
     return (
         <div className='home'>
             <div className='home-top-image'/>
@@ -19,14 +37,20 @@ const Home = () => {
             </div>
             <h1>Blogs</h1>
             <div className='home-posts'>
-                <Post
-                    username={'joesmith098'}
-                    avatar={''}
-                    image={''}
-                    title={'zoo'}
-                    post={'The quick brown fox jumped over the lazy dog'}
-                    blog={true}
-                />
+            {
+                Array.apply(0, Array(3)).map(function(x, i) {
+                    return (
+                        <Post
+                            username={'joesmith098'}
+                            avatar={''}
+                            image={''}
+                            title={'zoo'}
+                            post={'The quick brown fox jumped over the lazy dog'}
+                            blog={true}
+                        />
+                    )
+                })
+            }
             </div>
         </div>
     );
