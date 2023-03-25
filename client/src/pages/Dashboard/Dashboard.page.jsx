60de419d693175b833b288ca5debde76b1ba35db
CoderJ01 <buildingguy@sbcglobal.net>
2023-03-24 21:49:31 -0700

    import AccessDenied to Dashboard

--- client/src/pages/Dashboard/Dashboard.page.jsx
+++ client/src/pages/Dashboard/Dashboard.page.jsx
@@ -1,50 +1,51 @@
 import React from 'react';
 import './Dashboard.style.css'
+import AccessDenied from '../../components/AccessDenied/AcessDenied';
 
 const Dashboard = ({ user }) => {
     let id;
 
-    let currentUser = user || ' '
-
-    for(let i = 0; i < 100; i++) {
-        currentUser.reviews[i].title = currentUser?.reviews[i]?.title || ' '
-        currentUser.blogs[i].title = currentUser?.blogs[i]?.title || ' '
-    }
-
-    console.log(currentUser[50]);
+    console.log(user);
 
     return (
         <div className='dashboard'>
-            <div className='dashboard-top-bar'>
-                <h1>Your Dashboard</h1>
-            </div>
-            <div className='dashboard-bottom'>
-                <div className='dashboard-b-user-info'>
-                    <h2>Your Avatar</h2>
-                    <div className='dashboard-b-u-i-avatar'>
-                        <img alt='' src=''/>
-                    </div>
-                    <h2 style={{ marginTop: '7%'}}>Your Bio</h2>
-                    <div className='dashboard-b-u-i-bio'>
-                        <text>{currentUser.bio}</text>
-                    </div>
+        {
+            user.length === 0 ? 
+            (
+                <AccessDenied/>
+            ) : 
+            (
+                <>
+                <div className='dashboard-top-bar'>
+                    <h1>Your Dashboard</h1>
                 </div>
-                <div className='dashboard-b-buttons'>
-                    <button>See your reviews</button>
-                    <button>See your blog posts</button>
-                </div>
-                <div className='dashboard-b-posts-display'>
-                    <div className='dashboard-b-p-d-flex'>
-                        {
-                            currentUser
-                        }
-                        <div className='d-b-p-d-f-single-post'>
+                <div className='dashboard-bottom'>
+                    <div className='dashboard-b-user-info'>
+                        <h2>Your Avatar</h2>
+                        <div className='dashboard-b-u-i-avatar'>
                             <img alt='' src=''/>
-                            {/* <h3><a href={`/your-post/${id}`}>{currentUser.reviews[0].title}</a></h3> */}
+                        </div>
+                        <h2 style={{ marginTop: '7%'}}>Your Bio</h2>
+                        <div className='dashboard-b-u-i-bio'>
+                            <text>{user.bio}</text>
+                        </div>
+                    </div>
+                    <div className='dashboard-b-buttons'>
+                        <button>See your reviews</button>
+                        <button>See your blog posts</button>
+                    </div>
+                    <div className='dashboard-b-posts-display'>
+                        <div className='dashboard-b-p-d-flex'>
+                            <div className='d-b-p-d-f-single-post'>
+                                <img alt='' src=''/>
+                                {/* <h3><a href={`/your-post/${id}`}>{currentUser.reviews[0].title}</a></h3> */}
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+                </>
+            )
+        }
         </div>
     );
 }
