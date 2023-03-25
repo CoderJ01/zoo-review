95f39c97961021674cf67d144622765d74dcddf9
CoderJ01 <buildingguy@sbcglobal.net>
2023-03-24 18:06:57 -0700

    validate user in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Home from './pages/Home/Home.page';
 import Header from './components/Header/Header.component';
@@ -5,13 +6,42 @@ import Review from './pages/Review/Review.page';
 import Blog from './pages/Blog/Blog.page';
 import Dashboard from './pages/Dashboard/Dashboard.page';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import cookie from 'js-cookie';
+import axios from 'axios';
+
+const baseURL = 'http://localhost:3001';
 
 function App() {
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    const fetchUser = async() => {
+      try {
+        let userCookie = cookie.get('zelp-cookie');
+        axios.get(baseURL + '/api/users')
+        .then(
+            response => {
+                for(let i = 0; i < response.data.length; i++) {
+                    if(response.data[i].randomString === userCookie) {
+                          setUser(response.data[i]);
+                    }
+                }
+            }
+        );
+      }
+      catch(error) {
+        console.log(error);
+      }
+    }
+    fetchUser();
+  }
+  ,[]);
+
   return (
     <div className='App'>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Header/>}>
+          <Route path='/' element={<Header user={user}/>}>
             <Route index element={<Home/>}/>
             <Route path='/write-review' element={<Review/>}/>
             <Route path='/post-blog' element={<Blog/>}/>
