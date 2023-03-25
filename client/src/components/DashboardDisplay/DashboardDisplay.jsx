538612447838ee9af08d19a27b7cd77e33bce3db
CoderJ01 <buildingguy@sbcglobal.net>
2023-03-25 13:23:29 -0700

    create DashboardDisplay component

---
+++ client/src/components/DashboardDisplay/DashboardDisplay.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const DashboardDisplay = () => {
+    return (
+        <div className='dashboard-b-posts-display'>
+            <div className='d-b-p-d-f-single-post'>
+                <img alt='' src=''/>
+                {/* <h3><a href={`/your-post/${id}`}>{currentUser.reviews[0].title}</a></h3> */}
+            </div>
+        </div>
+    );
+}
+
+export default DashboardDisplay;
