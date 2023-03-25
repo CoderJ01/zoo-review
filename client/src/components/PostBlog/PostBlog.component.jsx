dd91d587453da43e95000216f748af5a80731eba
CoderJ01 <buildingguy@sbcglobal.net>
2023-03-25 15:01:55 -0700

    clean

--- client/src/components/PostBlog/PostBlog.component.jsx
+++ client/src/components/PostBlog/PostBlog.component.jsx
@@ -6,26 +6,10 @@ import '../PostReview/PostReview.style.css'
 const PostBlog = () => {
     const [title, setTitle] = useState('');
     const [content, setContent] = useState('');
-    const [file, setFile] = useState('');
+    // const [file, setFile] = useState('');
 
     const handleSubmit = (event) => {
         event.preventDefault();
-
-        const url = 'http://localhost:3000/write-review';
-        const formData = new FormData();
-
-        formData.append('file', file);
-        formData.append('fileName', file.name);
-
-        const config = {
-            headers: {
-                'content-type': 'multipart/form-data',
-            },
-        };
-
-        axios.post(url, formData, config).then((response) => {
-            console.log(response.data);
-        });
     }
 
     return (
@@ -39,10 +23,10 @@ const PostBlog = () => {
                 <label htmlFor='content'>Content:</label><br/>
                 <textarea type='text' name='content' value={content} onChange={(e) => setContent(e.target.value)}/>
             </div>
-            <div>
+            {/* <div>
                 <label htmlFor='file'></label><br/>
                 <input type='file' name='file' onChange={(e) => setFile(e.target.files[0])}/>
-            </div>
+            </div> */}
             <button type='submit'>+ New Blog</button>
         </form>
     );
