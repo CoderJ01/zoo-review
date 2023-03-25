cc3745ab7a9bd1e1fc76ead56ae06eaa66e3e9c3
CoderJ01 <buildingguy@sbcglobal.net>
2023-03-25 13:19:22 -0700

    clean

--- client/src/pages/Dashboard/Dashboard.page.jsx
+++ client/src/pages/Dashboard/Dashboard.page.jsx
@@ -12,33 +12,31 @@ const Dashboard = ({ user }) => {
             ) : 
             (
                 <div className='dashboard'>
-                <div className='dashboard-top-bar'>
-                    <h1>Your Dashboard</h1>
-                </div>
-                <div className='dashboard-bottom'>
-                    <div className='dashboard-b-user-info'>
-                        <h2>Your Avatar</h2>
-                        <div className='dashboard-b-u-i-avatar'>
-                            <img alt='' src=''/>
-                        </div>
-                        <h2 style={{ marginTop: '7%'}}>Your Bio</h2>
-                        <div className='dashboard-b-u-i-bio'>
-                            <text>{user.bio}</text>
-                        </div>
+                    <div className='dashboard-top-bar'>
+                        <h1>Your Dashboard</h1>
                     </div>
-                    <div className='dashboard-b-buttons'>
-                        <button>See your reviews</button>
-                        <button>See your blog posts</button>
-                    </div>
-                    <div className='dashboard-b-posts-display'>
-                        <div className='dashboard-b-p-d-flex'>
+                    <div className='dashboard-bottom'>
+                        <div className='dashboard-b-user-info'>
+                            <h2>Your Avatar</h2>
+                            <div className='dashboard-b-u-i-avatar'>
+                                <img alt='' src=''/>
+                            </div>
+                            <h2 style={{ marginTop: '7%'}}>Your Bio</h2>
+                            <div className='dashboard-b-u-i-bio'>
+                                <text>{user.bio}</text>
+                            </div>
+                        </div>
+                        <div className='dashboard-b-buttons'>
+                            <button>See your reviews</button>
+                            <button>See your blog posts</button>
+                        </div>
+                        <div className='dashboard-b-posts-display'>
                             <div className='d-b-p-d-f-single-post'>
                                 <img alt='' src=''/>
                                 {/* <h3><a href={`/your-post/${id}`}>{currentUser.reviews[0].title}</a></h3> */}
                             </div>
                         </div>
                     </div>
-                </div>
                 </div>
             )
         }
