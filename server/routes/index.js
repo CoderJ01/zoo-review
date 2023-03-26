a8f7a44b72cc24f92ad4c226648d92d7c61b878c
CoderJ01 <buildingguy@sbcglobal.net>
2023-03-25 18:24:16 -0700

    import route

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,11 +4,13 @@ const authRoutes = require('./auth');
 const homepageRoutes = require('./homepage');
 const dashboardRoutes = require('./dashboard');
 const postBlogRoutes = require('./post-blog');
+const postReviewRoutes = require('./post-review');
 
 router.use('/api', apiRoutes);
 router.use('/auth', authRoutes);
 router.use('/homepage', homepageRoutes);
 router.use('/dashboard', dashboardRoutes);
 router.use('/post-blog', postBlogRoutes);
+router.use('/post-review', postReviewRoutes);
 
 module.exports = router;
