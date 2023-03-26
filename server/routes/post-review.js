e049fd73cd7441087e313bd98382a2b76041a805
CoderJ01 <buildingguy@sbcglobal.net>
2023-03-25 18:23:14 -0700

    edit imports

--- server/routes/post-review.js
+++ server/routes/post-review.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const mongoose = require('mongoose');
-const ReviewSchema = require('../../models/Review');
+const ReviewSchema = require('../models/Review');
 const Review = mongoose.model('Review', ReviewSchema);
-const User = require('../../models/User');
-const Zoo = require('../../models/Zoo');
+const User = require('../models/User');
+const Zoo = require('../models/Zoo');
 const { retrieveSession } = require('../MongoDB/data');
 
 router.post('/:userId/:zooId', async (req, res) => {
