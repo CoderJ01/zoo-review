4f0862dbfbb9649b450e466fcf2d8de6358c98ee
CoderJ01 <buildingguy@sbcglobal.net>
2023-03-28 13:47:45 -0700

    axios GET to fecth blog (and user)

--- client/src/pages/SingleBlog/SingleBlog.page.jsx
+++ client/src/pages/SingleBlog/SingleBlog.page.jsx
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useEffect, useCallback } from 'react';
 import { useParams } from 'react-router';
+import axios from 'axios';
+
+const baseURL = 'http://localhost:3001';
 
 const SingleBlog = () => {
     const { blogId } = useParams();
+
+    const fetchBlogById = useCallback(async () => {
+        const id = blogId;
+
+        if(id) {
+            try {
+                const response = await axios.get(baseURL + `/single-blog/${id}`);
+                console.log(response.data);
+            }
+            catch(error) {
+                console.log(error);
+            }
+        }
+    }, [blogId]);
+
+    useEffect(() => {
+        fetchBlogById();
+    }, [fetchBlogById]);
 
     return (
         <></>
