03b1be9cc717b7f985982b6737eb7e94a95c8b36
CoderJ01 <buildingguy@sbcglobal.net>
2023-03-27 19:12:25 -0700

    structure jsx for review

--- client/src/pages/SingleReview/SingleReview.page.jsx
+++ client/src/pages/SingleReview/SingleReview.page.jsx
@@ -1,11 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import { displayRating } from '../../components/Post/Post.util';
 
 const baseURL = 'http://localhost:3001';
 
 const SingleReview = () => {
     const { reviewId } = useParams();
+    const [ review, setReview ] = useState([]);
+    const [ date, setDate ] = useState([]);
+
+    let rating = displayRating(review.data.rating);
 
     useEffect(() => {
         const id = reviewId;
@@ -14,7 +19,8 @@ const SingleReview = () => {
             const fetchReviewById = async () => {
                 try {
                     const response = await axios.get(baseURL + `/single-review/${id}`);
-                    console.log(response.data);
+                    setReview(response.data);
+                    setDate(response.data.data.updatedAt.substring());
                 }
                 catch(error) {
                     console.log(error);
@@ -24,8 +30,19 @@ const SingleReview = () => {
         } 
     }, [reviewId]);
 
+    console.log(review.data.updatedAt.toString().substring(0, 10));
+
     return (
-        <></>
+        <div className='single-review'>
+            <div className='single-review-display'>
+                <h1>{review.data.title}</h1>
+                <p>{review.data.content}</p>
+                {rating}
+                <p>{review.data.updatedAt.toString().substring(0, 10)}</p>
+                <p>{review.zoo}</p>
+                <p>{review.user}</p>
+            </div>
+        </div>
     );
 }
 
