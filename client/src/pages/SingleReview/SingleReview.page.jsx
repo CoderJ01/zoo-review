aa05c4fc54599a63861af9e2426bc43cdb795c0c
CoderJ01 <buildingguy@sbcglobal.net>
2023-03-27 19:37:07 -0700

    enhanced organization structure via adding div tags

--- client/src/pages/SingleReview/SingleReview.page.jsx
+++ client/src/pages/SingleReview/SingleReview.page.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import { displayRating } from '../../components/Post/Post.util';
@@ -8,42 +8,48 @@ const baseURL = 'http://localhost:3001';
 const SingleReview = () => {
     const { reviewId } = useParams();
     const [ review, setReview ] = useState([]);
-    const [ date, setDate ] = useState([]);
+
+    const fetchReviewById = useCallback(async () => {
+        const id = reviewId;
+        if(id) {
+            try {
+                const response = await axios.get(baseURL + `/single-review/${id}`);
+                console.log(response.data)
+                setReview(response.data);
+            }
+            catch(error) {
+                console.log(error);
+            }
+        }
+    }, [reviewId, setReview]);
+
+    useEffect(() => {
+        fetchReviewById();
+    }, [fetchReviewById]);
 
     let rating = displayRating(review.data.rating);
 
-    useEffect(() => {
-        const id = reviewId;
-
-        if(id) {
-            const fetchReviewById = async () => {
-                try {
-                    const response = await axios.get(baseURL + `/single-review/${id}`);
-                    setReview(response.data);
-                    setDate(response.data.data.updatedAt.substring());
-                }
-                catch(error) {
-                    console.log(error);
-                }
-            }
-            fetchReviewById();
-        } 
-    }, [reviewId]);
-
-    console.log(review.data.updatedAt.toString().substring(0, 10));
+    console.log(review.data);
 
     return (
         <div className='single-review'>
             <div className='single-review-display'>
-                <h1>{review.data.title}</h1>
-                <p>{review.data.content}</p>
-                {rating}
-                <p>{review.data.updatedAt.toString().substring(0, 10)}</p>
-                <p>{review.zoo}</p>
-                <p>{review.user}</p>
+                <div className='single-review-display-header'>
+                    <h1>{review.data.title}</h1>
+                    <p>{review.zoo}</p>
+                </div>
+                <div className='single-review-display-body'>
+                    <p>{review.data.content}</p>
+                    {rating}
+                </div>
+                <div className='single-review-display-body'>
+                    <p>{review.data.updatedAt.toString().substring(0, 10)}</p>
+                    <p>{review.user}</p> 
+                </div>
             </div>
         </div>
     );
 }
 
 export default SingleReview;
+
