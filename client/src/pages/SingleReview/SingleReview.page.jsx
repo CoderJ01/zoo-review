61a4e9033b1ec607eef4d60b5c43d2e7f16f5bd0
CoderJ01 <buildingguy@sbcglobal.net>
2023-03-27 18:30:10 -0700

    set up useParams for single-review page

--- client/src/pages/SingleReview/SingleReview.page.jsx
+++ client/src/pages/SingleReview/SingleReview.page.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const SingleReview = () => {
+    const { reviewId } = useParams();
+    console.log(reviewId);
     return (
         <></>
     );
