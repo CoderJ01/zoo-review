1bd5fea7ab5baf742c6f564129b3de5272fd825e
CoderJ01 <buildingguy@sbcglobal.net>
2023-03-27 18:40:05 -0700

    fetch infomation for a single review

--- client/src/pages/SingleReview/SingleReview.page.jsx
+++ client/src/pages/SingleReview/SingleReview.page.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
+
+const baseURL = 'http://localhost:3001';
 
 const SingleReview = () => {
     const { reviewId } = useParams();
-    console.log(reviewId);
+
+    useEffect(() => {
+        const id = reviewId;
+
+        if(id) {
+            const fetchReviewById = async () => {
+                try {
+                    const response = await axios.get(baseURL + `/single-review/${id}`);
+                    console.log(response.data);
+                }
+                catch(error) {
+                    console.log(error);
+                }
+            }
+            fetchReviewById();
+        } 
+    }, [reviewId]);
+
     return (
         <></>
     );
