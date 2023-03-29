54b1d52303cb1c09927291b4783a865e053e0ad5
CoderJ01 <buildingguy@sbcglobal.net>
2023-03-28 21:00:51 -0700

    import

--- client/src/components/ZooReview/ZooReview.component.jsx
+++ client/src/components/ZooReview/ZooReview.component.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useCallback } from 'react';
 import { displayRating, defaultProfileImage } from '../Post/Post.util';
 import axios from 'axios';
 import '../../pages/SingleReview/SingleReview.style.css';
+import './ZooReview.style.css';
 
 const baseURL = 'http://localhost:3001';
 
