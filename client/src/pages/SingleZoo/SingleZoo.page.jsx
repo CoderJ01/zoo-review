e98d3e46fdd153826955f6226c7c11b491fb5039
CoderJ01 <buildingguy@sbcglobal.net>
2023-03-28 19:13:55 -0700

    structure jsx for single-zoo page

--- client/src/pages/SingleZoo/SingleZoo.page.jsx
+++ client/src/pages/SingleZoo/SingleZoo.page.jsx
@@ -5,7 +5,16 @@ const SingleZoo = () => {
     const { zooId } = useParams();
     console.log(zooId);
     return (
-        <></>
+        <div className='single-zoo'>
+            <h1>Name of Zoo</h1>
+            <div className='single-zoo-photo'>
+                <img alt='' src=''></img>
+            </div>
+            <h2>Reviews</h2>
+            <div className='sz-list-of-reviews'>
+
+            </div>
+        </div>
     );
 }
 
