b100992295e894ffa445c11fedec59c0ab770544
CoderJ01 <buildingguy@sbcglobal.net>
2023-03-28 19:39:25 -0700

    fetch zoo data

--- client/src/pages/SingleZoo/SingleZoo.page.jsx
+++ client/src/pages/SingleZoo/SingleZoo.page.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useEffect, useCallback } from 'react';
 import { useParams } from 'react-router';
+import axios from 'axios';
+
+const baseURL = 'http://localhost:3001';
 
 const SingleZoo = () => {
     const { zooId } = useParams();
     console.log(zooId);
+
+    const fetchZooById = useCallback(async () => {
+        const id = zooId;
+
+        if(id) {
+            try {
+                const response = await axios.get(baseURL + `/single-zoo/${id}`);
+                console.log(response.data);
+            }
+            catch(error) {
+                console.log(error);
+            }
+        }
+    }, [zooId]);
+
+    useEffect(() => {
+        fetchZooById();
+    }, [fetchZooById]);
+
     return (
         <div className='single-zoo'>
             <h1>Name of Zoo</h1>
