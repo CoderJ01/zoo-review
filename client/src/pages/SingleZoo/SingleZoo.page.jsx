e563859fa59b502d0ef5f7a293a0f3bdc79a66e6
CoderJ01 <buildingguy@sbcglobal.net>
2023-03-28 18:47:31 -0700

    retrieve ids for zoos with useParams hook

--- client/src/pages/SingleZoo/SingleZoo.page.jsx
+++ client/src/pages/SingleZoo/SingleZoo.page.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useParams } from 'react-router';
 
 const SingleZoo = () => {
+    const { zooId } = useParams();
+    console.log(zooId);
     return (
         <></>
     );
