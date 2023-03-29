1baca81a42ca0339d8bd9a01041e121979fe582a
CoderJ01 <buildingguy@sbcglobal.net>
2023-03-29 14:55:27 -0700

    clean

--- client/src/components/Post/Post.component.jsx
+++ client/src/components/Post/Post.component.jsx
@@ -1,37 +1,17 @@
-import React, { useState, useEffect} from 'react';
+import React from 'react';
 import './Post.style.css'
 import { displayRating, defaultProfileImage, blogImage, reviewImage } from './Post.util';
-import axios from 'axios';
-
-const baseURL = 'http://localhost:3001';
 
 const Post = ({ post, blog = false }) => {
-    const [zooName, setZooName] =useState('');
 
     let ratingDisplay
+
     if(post.rating) {
         ratingDisplay = displayRating(post.rating);
     }
     else {
         ratingDisplay = '';
     }
-
-    useEffect(() => {
-        const id = post.zoo;
-
-        if(id) {
-            const fetchZooImage = async () => {
-                try {
-                    const response = await axios.get(baseURL + `/api/zoos/${id}`);
-                    setZooName(response.data.name);
-                }
-                catch(error) {
-                    console.log(error);
-                }
-            }
-            fetchZooImage();
-        }
-    }, [post.zoo])
 
     return (
         <div className='post'>
@@ -52,26 +32,17 @@ const Post = ({ post, blog = false }) => {
             }
             </div>
             <div className='post-info'>
-                {
-                    !blog ? 
-                    (
-                        <>
-                            <h1>
-                                <text>{zooName}: <a href={`review/${post._id}`} target='_blank' rel='noreferrer'>{[post.title]}</a></text>
-                            </h1>
-                            <div className='post-info-rating'>
-                                {ratingDisplay}
-                            </div>
-                        </>
-                    ) : 
-                    (
-                        <>
-                            <h1><a href={`blog/${post._id}`} target='_blank' rel='noreferrer'>{post.title}</a></h1>
-                            <p>{post.thumbs} likes</p>
-                        </>
-                    )
-                }
-                
+            <h1><a href={`review/${post._id}`} target='_blank' rel='noreferrer'>{[post.title]}</a></h1>
+            <div className='post-info-rating'>{ratingDisplay}</div>
+            {
+                !blog ? 
+                (
+                    ''
+                ) : 
+                (
+                    <p>{post.thumbs} likes</p>
+                )
+            }
             </div>
         </div>
     );
