1dfdf648ab96ef4052868c2335a579e5539b2144
CoderJ01 <buildingguy@sbcglobal.net>
2023-03-29 17:40:52 -0700

    create PUT route in backend to process blog 'like'

--- server/routes/single-blog.js
+++ server/routes/single-blog.js
@@ -15,4 +15,26 @@ router.get('/:id', async (req, res) => {
     });
 });
 
+router.put('/like/:userId/:blogId', async (req, res) => {
+    const user = await User.findOne({ _id: req.params.userId });
+    console.log(user);
+    
+    if(!user.likedBlogs.includes(req.params.blogId)) {
+        user.likedBlogs.push(req.params.blogId);
+        if(user.dislikedBlogs.includes(req.params.blogId)) {
+            user.dislikedBlogs.pull(req.params.blogId);
+        }
+    }
+    else {
+        user.likedBlogs.pull(req.params.blogId);
+    }
+
+    user.save();
+
+    res.send({
+        liked: user.likedBlogs,
+        disliked: user.dislikedBlogs
+    });
+});
+
 module.exports = router;
