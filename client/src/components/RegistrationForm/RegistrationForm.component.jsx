cf0eaca1060f1d93d09b3b313610cc0e341dd14d
CoderJ01 <buildingguy@sbcglobal.net>
2023-04-03 16:14:20 -0700

    clean

--- client/src/components/RegistrationForm/RegistrationForm.component.jsx
+++ client/src/components/RegistrationForm/RegistrationForm.component.jsx
@@ -32,29 +32,10 @@ const RegistrationForm = ({ showSignup, handleCloseSignup }) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [bio, setBio] = useState('');
-    // const [file, setFile] = useState('');
 
     const handleSubmit = async (event) => {
         event.preventDefault();
-        // const formData = new FormData();
-        // formData.append('file', formData);
-
-        // try {
-        //     const response = await axios.post(
-        //         `${baseURL}/users/register`,
-        //         formData, {
-        //             headers: {
-        //                 'Content-Type': 'multipart/form-data'
-        //             }
-        //         }
-        //     );
-        //     console.log(response.data);
-        //     console.log(response);
-        // }
-        // catch(error) {
-        //     console.log(error);
-        // }
-
+       
         axios.post(baseURL + '/auth/register', 
         {
             firstname: firstName,
@@ -91,10 +72,6 @@ const RegistrationForm = ({ showSignup, handleCloseSignup }) => {
         );
     }
 
-    // const handleFileSelect = (event) => {
-    //     setFile(event.target.files[0]);
-    // }
-
     return (
         <Modal show={showSignup} onHide={handleCloseSignup}>
             <Modal.Header style={modalHeaderStyle}>
@@ -126,10 +103,6 @@ const RegistrationForm = ({ showSignup, handleCloseSignup }) => {
                         <label htmlFor='bio'>Bio:</label><br/>
                         <textarea maxLength={200} type='text' name='bio' value={bio} onChange={(e) => setBio(e.target.value)}/>
                     </div>
-                    {/* <div>
-                        <label htmlFor='file'>Avatar:</label><br/>
-                        <input type='file' name='file' onChange={handleFileSelect}/>
-                    </div> */}
                     <Button type='submit' style={buttonStyle} onClick={handleCloseSignup}>
                         Sign up
                     </Button>
