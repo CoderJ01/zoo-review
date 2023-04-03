9b0e29003e07b98d3e3680643ee9bda722b1bebb
CoderJ01 <buildingguy@sbcglobal.net>
2023-04-03 15:47:53 -0700

    restructure code

--- client/src/components/Post/Post.component.jsx
+++ client/src/components/Post/Post.component.jsx
@@ -25,44 +25,43 @@ const Post = ({ post, blog = false }) => {
                     <img alt='' src={defaultProfileImage}/>
                 </div>
             </div>
-            <div className='post-picture'>
             {
                 !blog ? 
                 (
-                    <img alt='' src={reviewImage}></img>
+                    <>
+                        <div className='post-picture'>
+                            <img alt='' src={reviewImage}></img> 
+                        </div>
+                        <div className='post-info'>
+                            <h1><a href={`review/${post._id}`} target='_blank' rel='noreferrer'>{[post.title]}</a></h1>
+                            <div className='post-info-rating'>{ratingDisplay}</div>
+                        </div>
+                    </>
                 ) : 
                 (
-                    <img alt='' src={blogImage}></img>
-                )
-            }
-            </div>
-            {
-                !blog ? 
-                (
-                    <div className='post-info'>
-                        <h1><a href={`review/${post._id}`} target='_blank' rel='noreferrer'>{[post.title]}</a></h1>
-                        <div className='post-info-rating'>{ratingDisplay}</div>
-                    </div>
-                ) : 
-                (
-                    <div className='post-info'>
-                        <h1><a href={`blog/${post._id}`} target='_blank' rel='noreferrer'>{[post.title]}</a></h1>
-                        {
-                            post.thumbs < 1 ? 
-                            (
-                                ''
-                            ) : 
-                            (
-                                post.thumbs === 1 ? 
+                    <>
+                        <div className='post-picture'>
+                            <img alt='' src={blogImage}></img> 
+                        </div>
+                        <div className='post-info'>
+                            <h1><a href={`blog/${post._id}`} target='_blank' rel='noreferrer'>{[post.title]}</a></h1>
+                            {
+                                post.thumbs < 1 ? 
                                 (
-                                    <p>{post.thumbs} like</p>
+                                    ''
                                 ) : 
                                 (
-                                    <p>{post.thumbs} likes</p>
+                                    post.thumbs === 1 ? 
+                                    (
+                                        <p>{post.thumbs} like</p>
+                                    ) : 
+                                    (
+                                        <p>{post.thumbs} likes</p>
+                                    )
                                 )
-                            )
-                        }
-                    </div>
+                            }
+                        </div>
+                    </>
                 )
             }
         </div>
