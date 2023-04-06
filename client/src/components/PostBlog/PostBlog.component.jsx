873a17dddc51f4a1e504cb718c2949ebeec1d7a0
CoderJ01 <buildingguy@sbcglobal.net>
2023-04-05 19:19:14 -0700

    enable image to be uploaded to blog

--- client/src/components/PostBlog/PostBlog.component.jsx
+++ client/src/components/PostBlog/PostBlog.component.jsx
@@ -9,11 +9,54 @@ import { baseURL } from '../../URLs/urls';
 
 // other imports
 import axios from 'axios';
+import { ref, uploadBytes, listAll, getDownloadURL } from 'firebase/storage';
+import { storage } from '../../config/firebase';
+import { v4 } from 'uuid';
 
 const PostBlog = ({ user }) => {
     const [title, setTitle] = useState('');
     const [content, setContent] = useState('');
     const [count, setCount] = useState(0);
+    const [imageUpload, setImageUpload] = useState(null);
+    const [imageUrl, setImageUrl] = useState('');
+    const [confirmed, setConfirmed] = useState(false);
+
+    const uploadImage = (e) => {
+        setImageUpload(e.target.files[0]);
+        alert('The image has been uploaded! Confirm the upload, then wait a few seconds to submit the blog!');
+    }
+
+    const confirmUpload = () => {
+
+        if(imageUpload != null) {
+            setConfirmed(true);
+            const imageRef = ref(storage, `images/blogs/${imageUpload.name + v4()}`);
+
+            uploadBytes(imageRef, imageUpload)
+            .then(() => {
+                alert('Image confirmed! Wait a few seconds for it to process!');
+            })
+            .catch(error => {
+                console.log(error);
+            });
+        }
+        else {
+            alert('You have not uploaded an image!');
+            window.location.reload(false);
+            return;
+        }
+
+        listAll(imageListRef)
+        .then(response => {
+            response.items.forEach(item => {
+                getDownloadURL(item).then(url => {
+                    setImageUrl(url);
+                });
+            })
+        });
+    }
+
+    const imageListRef = ref(storage, 'images/blogs/');
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -28,15 +71,22 @@ const PostBlog = ({ user }) => {
             return;
         }
 
+        if(imageUpload != null && confirmed === false) {
+            alert('The upload needs to be confirmed so that image will process successfully!');
+            return;
+        }
+
         axios.post(baseURL + `/post-blog/${user._id}`, 
             {
                 title: title,
-                content: content
+                content: content,
+                image: imageUrl
             }
         )
         .then(
             response => {
                 alert(response.data.msg);
+                window.location.reload(false);
             },
         )
         .catch(
@@ -81,6 +131,16 @@ const PostBlog = ({ user }) => {
                 )
             }
                 <textarea type='text' name='content' value={content} onChange={handleContent}/>
+            </div>
+            <div className='pf-upload-image'>
+                <input 
+                    type='file'
+                    onChange={uploadImage} 
+                />
+            </div>
+            <div className='pf-confirm-upload'>
+                <label htmlFor='radio'>Confirm image</label><br/>
+                <input type='radio' name='radio' onChange={confirmUpload}/>
             </div>
             <button type='submit'>+ New Blog</button>
         </form>
