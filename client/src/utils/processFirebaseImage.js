bd1a72898e0086516576da56cac74f60e4ed8637
CoderJ01 <buildingguy@sbcglobal.net>
2023-04-07 21:01:12 -0700

    create util to for image storage

--- client/src/utils/processFirebaseImage.js
+++ client/src/utils/processFirebaseImage.js
@@ -1,5 +1,7 @@
 import { cloudString } from './cloudString';
-import { ref, deleteObject, getStorage } from 'firebase/storage';
+import { ref, uploadBytes, deleteObject, getStorage } from 'firebase/storage';
+import { storage } from '../config/firebase';
+import { v4 } from 'uuid';
 
 export const deleteFirebaseImage = async (image) => {
     if(image.includes(cloudString)) {
@@ -8,5 +10,25 @@ export const deleteFirebaseImage = async (image) => {
         deleteObject(imageRef).then(() => {
             console.log('Old avatar has been deleted!');
         }).catch(error => console.log(error))
+    }
+}
+
+export const storeFirebaseImage = async (imageUpload, setConfirmed, directory) => {
+    if(imageUpload != null) {
+        setConfirmed(true);
+        const imageRef = ref(storage, `images/${directory}/${imageUpload.name + cloudString + v4()}`);
+
+        uploadBytes(imageRef, imageUpload)
+        .then(() => {
+            alert('Image confirmed! Wait a few seconds for it to process!');
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    }
+    else {
+        alert('You have not uploaded an image!');
+        window.location.reload(false);
+        return;
     }
 }
