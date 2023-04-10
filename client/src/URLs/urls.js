b696346877c9ead7d6dfae335350c02e972b11c8
CoderJ01 <buildingguy@sbcglobal.net>
2023-04-09 19:07:20 -0700

    change url links for client

--- client/src/URLs/urls.js
+++ client/src/URLs/urls.js
@@ -1,3 +1,3 @@
-export const baseURL = 'http://localhost:3001';
+export const baseURL = process.env.REACT_APP_DEPLOYED_BACKEND;
 
-export const baseURL_frontend = 'http://localhost:3000';
+export const baseURL_frontend = process.env.REACT_APP_DEPLOYED_FRONTEND;
