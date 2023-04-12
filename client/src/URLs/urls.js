01fd1c98e66bfc01495080b01281193d2571a729
CoderJ01 <buildingguy@sbcglobal.net>
2023-04-11 20:15:25 -0700

    alternate url

--- client/src/URLs/urls.js
+++ client/src/URLs/urls.js
@@ -1,3 +1,3 @@
-export const baseURL = process.env.REACT_APP_DEPLOYED_BACKEND || 'http://localhost:3000';
+export const baseURL = process.env.REACT_APP_DEPLOYED_BACKEND || 'http://localhost:3001';
 
-export const baseURL_frontend = process.env.REACT_APP_DEPLOYED_FRONTEND;
+export const baseURL_frontend = process.env.REACT_APP_DEPLOYED_FRONTEND || 'http://localhost:3000';
