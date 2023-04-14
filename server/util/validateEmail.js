da73e0b37f0f8582d0374fde9dd1fb3a37f445a7
CoderJ01 <buildingguy@sbcglobal.net>
2023-04-14 16:53:35 -0700

    create function to send email to new user

---
+++ server/util/validateEmail.js
@@ -0,0 +1,42 @@
+const nodemailer = require('nodemailer');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const validateEmail = (email) => {
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.PASSWORD
+        }
+    });
+      
+    const token = jwt.sign({
+            data: 'Token Data'  
+        }, process.env.SECRET_KEY, { expiresIn: '10m' }  
+    );    
+      
+    const mailConfigurations = {
+        from: 'joshua001zelp@gmail.com',
+        to: email,
+        subject: 'Email Verification',
+        text: `Hi! There, You have recently visited 
+               Zelp and entered your email.
+               Please follow the given link to verify your email
+               http://localhost:3000/verify/${token} 
+               Thanks!`
+          
+    };
+      
+    transporter.sendMail(mailConfigurations, function(error, info){
+        if (error) {
+            throw Error(error);
+        }
+        else {
+            console.log('Email Sent Successfully');
+            console.log(info);
+        }
+    });
+}
+
+export default validateEmail;
