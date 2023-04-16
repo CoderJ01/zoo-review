5133e56a8a7362250e031cb7e6e1cbffbee743e3
CoderJ01 <buildingguy@sbcglobal.net>
2023-04-15 19:59:16 -0700

    create conditional to account for registration vs. update

--- server/util/validateEmail.js
+++ server/util/validateEmail.js
@@ -2,7 +2,9 @@ const nodemailer = require('nodemailer');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-const validateEmail = (email) => {
+const validateEmail = (email, newUser) => {
+    let message;
+
     const transporter = nodemailer.createTransport({
         service: 'gmail',
         host: 'smtp.gmail.com',
@@ -17,18 +19,25 @@ const validateEmail = (email) => {
     const token = jwt.sign({
             data: 'Token Data'  
         }, process.env.SECRET_KEY, { expiresIn: '10m' }  
-    );    
+    ); 
+    
+    if(newUser === true) {
+        message = `Hi! There, You have recently visited 
+        Zelp and entered your email.
+        Please follow the given link to verify your email
+        ${process.env.REGISTRATION}/auth/verify/${email}/${token} 
+        Thanks!`
+    }
+    else if(newUser === false) {
+        message = `You have recently submitted a request to update your email.
+        Please verify the update by clicking on the link:  ${process.env.REGISTRATION}/auth/verify/${email}/${token}`
+    }
       
     const mailConfigurations = {
         from: 'joshua001zelp@gmail.com',
         to: email,
         subject: 'Email Verification for Zelp',
-        text: `Hi! There, You have recently visited 
-               Zelp and entered your email.
-               Please follow the given link to verify your email
-               ${process.env.REGISTRATION}/auth/verify/${email}/${token} 
-               Thanks!`
-          
+        text: message
     };
       
     transporter.sendMail(mailConfigurations, function(error, info){
