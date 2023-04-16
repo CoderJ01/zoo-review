d71f43edcaa17b251703798a9fe4b7af49b0b6b9
CoderJ01 <buildingguy@sbcglobal.net>
2023-04-15 20:04:19 -0700

    update conditional

--- server/util/validateEmail.js
+++ server/util/validateEmail.js
@@ -2,7 +2,7 @@ const nodemailer = require('nodemailer');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-const validateEmail = (email, newUser) => {
+const validateEmail = (email, reason) => {
     let message;
 
     const transporter = nodemailer.createTransport({
@@ -21,16 +21,19 @@ const validateEmail = (email, newUser) => {
         }, process.env.SECRET_KEY, { expiresIn: '10m' }  
     ); 
     
-    if(newUser === true) {
+    if(reason === 'newUser') {
         message = `Hi! There, You have recently visited 
         Zelp and entered your email.
         Please follow the given link to verify your email
         ${process.env.REGISTRATION}/auth/verify/${email}/${token} 
         Thanks!`
     }
-    else if(newUser === false) {
+    else if(reason === 'updateEmail') {
         message = `You have recently submitted a request to update your email.
-        Please verify the update by clicking on the link:  ${process.env.REGISTRATION}/auth/verify/${email}/${token}`
+        Please verify the update by clicking on the link: ${process.env.REGISTRATION}/auth/verify/${email}/${token}`
+    }
+    else if(reason === 'loginAttempt') {
+        message = `You have attempted to login. Verify your email by clicking on the link: ${process.env.REGISTRATION}/auth/verify/${email}/${token}`;
     }
       
     const mailConfigurations = {
